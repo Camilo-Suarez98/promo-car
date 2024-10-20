24b413b98ab207806b9d70dd63736ee9e4699cc8
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-10-19 21:35:18 -0500

    create styles for main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import RegistrationForm from "@/components/RegistrationForm";
 import Image from "next/image";
+
+import RegistrationForm from "@/components/RegistrationForm";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex gap-8 row-start-2 justify-center items-center sm:items-start">
+        <Image src="/car.png" alt="Imagén de carros" width={500} height={200} />
         <RegistrationForm />
       </main>
     </div>
