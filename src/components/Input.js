74817f92478f44d17495052426cbb3bec21110df
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-10-20 20:13:00 -0500

    include value and handlechange in inputs

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,14 +3,23 @@ import React from 'react';
 const Input = ({
   nameLabel,
   placeholder,
+  inputName,
   type,
   value,
   onChange
 }) => {
   return (
-    <label className='flex flex-col'>
+    <label className='flex flex-col text-[#9ca3af] my-2'>
       {nameLabel}
-      <input className='text-black' type={type} placeholder={placeholder} name={nameLabel.toLowerCase()} required />
+      <input
+        className='py-3 px-4 bg-[#111827] text-[#f3f4f6] rounded-md border-[1px] border-[#f3f4f6] focus:border-[#1a80a3d9]'
+        type={type}
+        placeholder={placeholder}
+        name={inputName}
+        value={value}
+        onChange={onChange}
+        required
+      />
     </label>
   )
 }
