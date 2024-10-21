6e096dfeb83644eab0f8251c7b52266dfd703793
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-10-20 20:11:22 -0500

    change tags to layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
-import Image from "next/image";
-
 import RegistrationForm from "@/components/RegistrationForm";
+import MainText from "@/components/MainText";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex gap-8 row-start-2 justify-center items-center sm:items-start">
-        <Image src="/car.png" alt="Imagén de carros" width={500} height={200} />
-        <RegistrationForm />
-      </main>
+    <div className="w-full flex flex-col items-center lg:flex-row lg:gap-7">
+      <MainText />
+      <RegistrationForm />
     </div>
   );
 }
