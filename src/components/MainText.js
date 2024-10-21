ed3f9ffabf36456bdfd8f703c8e74272718a5eec
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-10-20 20:11:43 -0500

    include information in form view

---
+++ src/components/MainText.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from "next/image";
+
+const MainText = () => {
+  return (
+    <div className='text-center mb-5 lg:text-left'>
+      <h1 className="text-3xl font-bold mb-4 lg:text-4xl">¡Regístrate para participar en el sorteo!</h1>
+      <p className="text-gray-400 lg:text-2xl">
+        Llena el siguiente formulario con tus datos para obtener tu código de participación.
+        Solo necesitas unos minutos para completar el registro. ¡Suerte!
+      </p>
+      <Image
+        className='m-auto lg:m-0 lg:w-full'
+        src="/car.png"
+        alt="Imagén de carros"
+        width={500}
+        height={200}
+      />
+    </div>
+  );
+};
+
+export default MainText;
